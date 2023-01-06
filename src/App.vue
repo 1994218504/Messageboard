99f9e6e2b9a014136ea1827502a8f6a671c9c893
liuguanghui <1994218504@qq.com>
2023-01-06 19:36:56 +0800

    预加载

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
+    <!-- 路由切换页面就是跳转该标签 -->
     <router-view />
   </div>
 </template>
+<script>
+import logger from './js/logger'
+
+export default {
+  mounted() {
+    logger.debug('根组件，顶级作用域======================》')
+    if (top != window) {
+      logger.debug('根组件，顶级作用域======================》=========')
+      top.postMessage('loaded', '*')
+    }
+  },
+}
+</script>
 <style scoped>
 @import url('./css/iconfont/iconfont.css');
 @import url('./css/common.css');
